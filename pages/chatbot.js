6910b4728c2db24a14e09f5fe124bc7ac7c354dc
KushalK04 <Kushal06Kumar@gmail.com>
2023-04-23 19:55:25 -0700

    ai

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -16,7 +16,7 @@ export default function ChatBot() {
     const url = "https://api.openai.com/v1/chat/completions";
     const headers = {
       "Content-type": "application/json",
-      Authorization: `Bearer ${"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"}`,
+      Authorization: `Bearer ${process.env.API_KEY}`,
     };
     const data = {
       model: "gpt-3.5-turbo-0301",
