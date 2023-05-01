b1c44d5981d876d8840a78e77322059efdf002e9
schwendyjr <113371678+schwendyjr@users.noreply.github.com>
2023-05-01 15:02:17 -0700

    logo animation component

---
+++ Components/LogoAnimation/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import styles from '../LogoAnimation/LogoAnimation.module.css'
+import { useRouter } from 'next/router'
+import LogoAnimation from '../public/SwindleLogoAnimation.mp4'
+
+
+export default function LogoAnimation() {
+
+    return (
+        <>
+            <video src={LogoAnimation} autoPlay loop muted className={styles.animation} />
+
+
+        </>
+    )
+}
